3e99707b2a0de5b9cb33873aa4ddbea8f46db67c
Pmparekh2000 <pmp.tech2000@gmail.com>
2020-09-06 02:07:35 -0700

    Added Pillow, Product Model and Category Model

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    # Here unique implies the creation of an index
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name    
+    
+class Product(models.Model):
+
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True)
+    # slug is basically used for building urls on a field
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    description = models.TextField(blank=True)
+    # The above two arguments "blank=True" makes the fields optional
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name',)
+        index_together = (('id', 'slug'),)
+        # index_together is used to make good index so as to improve the query set performance
+
+    def __str__(self):
+        return self.name    
